2199a7ee81d11a18806e6b9a67ac3b438a91ce34
quentinglorieux <quentin.glorieux@gmail.com>
2022-10-27 14:05:47 +0200

    Updated simply-static-1-1666871686

--- wp-content/plugins/rbw-plugin-pro/search-bar/search.js
+++ wp-content/plugins/rbw-plugin-pro/search-bar/search.js
@@ -1,13 +1,26 @@
 
-function show_articles(article_cpt_class,good_post_ids){
+function show_articles(){
     //  article_cpt_class is the class name given on elementor to the section (article)
     //  good_post_ids is an array of the post with id that we want to keep on display
-    var list = document.getElementsByClassName(article_cpt_class);
+
+
+    cpt=document.getElementsByClassName('rbw-search')[0].getAttribute('cpt');
+    search_bar_id='rbw-search-input-'+cpt;
+    input=document.getElementById(search_bar_id)
+    input_val=input.value.toLowerCase()
+
+    search_data_name=document.getElementsByClassName('rbw-autocomplete')[0].getAttribute('searching-data');
+    good_id=get_satisfying_post_id(input_val,search_data_name);
+    // console.log('show_article');
+    // console.log('cpt',cpt, 'input', input, 'input_val',input_val )
+    // console.log('search_data_name', search_data_name )
+    // console.log('good_id',good_id);
+    var list = document.getElementsByClassName(cpt);
     // console.log(list);
     for (let item of list) {
         id=item.getAttribute('data-dce-post-id')
         // console.log(id)
-        if(good_post_ids.includes(id) ){
+        if(good_id.includes(id) ){
             item.style.display="block";
             item.style.position="relative";
         }else{
@@ -22,8 +35,7 @@ jQuery( document ).ready( function($) {
     function filter_article(inp,cpt,search_data) {
         inp.addEventListener("input", function(e) {
             var  val = this.value;
-            var post_ids= get_satisfying_post_id(val,search_data);
-            show_articles(cpt, post_ids);
+            show_articles();
         });
        
         
@@ -31,23 +43,26 @@ jQuery( document ).ready( function($) {
 
     if (document.getElementsByClassName('rbw-search')){
         cpt=document.getElementsByClassName('rbw-search')[0].getAttribute('cpt');
-        search_data=document.getElementsByClassName('rbw-autocomplete')[0].getAttribute('searching-data');
+        search_data_name=document.getElementsByClassName('rbw-autocomplete')[0].getAttribute('searching-data');
         search_bar_id='rbw-search-input-'+cpt;
-        console.log(searchdata);
-        filter_article(document.getElementById(search_bar_id),cpt,search_data);
-
-        
+        // console.log(searchdata);
+        filter_article(document.getElementById(search_bar_id),cpt,search_data_name); 
     }    
 });
   
-function get_satisfying_post_id(input_val,search_data){
-    data=searchdata[search_data];
+function get_satisfying_post_id(input_val,search_data_name){
+    
+    data=searchdata[search_data_name];
+    // console.log(searchdata);
+    // console.log(search_data_name);
+
     // search_data is a keyword that has to be added in elementor short code eg. [\search_bar searching_data='teams'] 
     // searchdata is an array send by localisation in search.php This part cannot be automatised, one has to decide what 
         // data to search in each case. 
     //  data is of the form {id_of_post:{data_type: array() }}
     good_id=[]
-    console.log(data);
+    
+
 
     for (var id in data){
         is_good=0
